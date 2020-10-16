b0a5fdc1b7ac44c6d5c89cfe44dd7567c4ac9bbf
Jason Robitaille <robitaille88@gmail.com>
2020-10-15 18:12:36 -0700

    Support manifest/serviceworker generation and image optimization

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,21 @@
  */
 
 module.exports = {
-  plugins: ['gatsby-plugin-styled-components']
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'LGSVL Simulator',
+        short_name: 'LGSVL Simulator',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'src/images/icon.png'
+      }
+    },
+    'gatsby-plugin-offline'
+  ]
 };
