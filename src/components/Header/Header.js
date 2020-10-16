04d0b9bc694637d9aa7158e2b4838dcbaa00c6e0
Jason Robitaille <robitaille88@gmail.com>
2020-10-15 17:43:36 -0700

    Switched to typescript and added styled-components

--- src/components/Header/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import styles from "./Header.module.css";
-
-const Header = ({ children }) => {
-    return (<div className={styles.header}>{children}</div>);
-};
-
-export { Header };
