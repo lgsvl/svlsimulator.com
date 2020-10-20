253ce531114831e032eb60a645172826c41a52db
Blake Stephens <blake.stephens@lge.com>
2020-10-20 15:08:08 -0700

    Marginally cleaner spacing and Theme provider

--- src/context/Theme.tsx
+++ src/context/Theme.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { ThemeProvider as MaterialThemeProvider } from '@material-ui/core/styles';
-import { theme } from '../styles/theme';
+import { ThemeProvider as MuiThemeProvider, ThemeProviderProps } from '@material-ui/core/styles';
+import { theme as defaultTheme } from '../styles/theme';
 
-interface IThemeProvider {
-  children: React.ReactNode;
-}
-
-export const ThemeProvider: React.FC<IThemeProvider> = ({ children }) => (
-  <MaterialThemeProvider theme={theme}>{children}</MaterialThemeProvider>
+export const ThemeProvider = ({ children, theme = defaultTheme }: ThemeProviderProps) => (
+  <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
 );
--- src/utils/addSpacing.ts
+++ src/utils/addSpacing.ts
@@ -1,14 +1,12 @@
 import { styled as muiStyled } from '@material-ui/core/styles';
 import { spacing, SpacingProps } from '@material-ui/system';
 
-// Add the `spacing` props to the component
-//const addSpacing = (Comp: React.FC) => muiStyled(Comp)(spacing) as React.FC<any>;
+type SpacedComponent<T> = React.FC<T & SpacingProps & {
+  component?: React.ComponentType | keyof JSX.IntrinsicElements
+}>;
 
-function addSpacing<T>(Comp: React.FC<T>) {
-  return muiStyled(Comp)(spacing) as React.FC<
-    T & SpacingProps & { component?: React.ComponentType | keyof JSX.IntrinsicElements }
-  >;
-}
+// Add the `spacing` props to the component
+const addSpacing = <T>(Comp: React.FC<T>) => muiStyled(Comp)(spacing) as SpacedComponent<T>;
 
 export default addSpacing;
 export { addSpacing };
