be18854dd0dc051928c24c443e2542e6e8d977eb
Blake Stephens <blake.stephens@lge.com>
2020-10-20 14:15:35 -0700

    Fixed useMeasurable typing, and Theme casing

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 import PageContextProvider from '../context/Page';
 import Header from './Header';
