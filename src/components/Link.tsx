dc63535f1fb01f05d56b39b1435511a1a676b824
Jason Robitaille <robitaille88@gmail.com>
2020-10-20 13:24:26 -0700

    Resolve type issues with Link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { GatsbyLinkProps } from 'gatsby';
 import { Link as GatsbyLink } from 'gatsby-plugin-react-i18next';
-import MuiLink from '@material-ui/core/Link';
+import MuiLink, { LinkBaseProps } from '@material-ui/core/Link';
+
+type I18nGatsbyLinkProps = GatsbyLinkProps<HTMLAnchorElement> & { language?: string };
+
+const FwdLink = React.forwardRef((props: I18nGatsbyLinkProps, ref) => (
+  <GatsbyLink {...props} ref={ref as React.Ref<HTMLAnchorElement>} />
+));
 
 // Gatsby bug needs a custom thing to work...
 // https://github.com/gatsbyjs/gatsby/issues/16682
@@ -11,7 +17,7 @@ import MuiLink from '@material-ui/core/Link';
 // and partiallyActive, destructure the prop here and
 // pass it only to GatsbyLink
 // const Link: React.FC<Omit<GatsbyLinkProps<{}>, 'ref'>> = ({ children, to, activeClassName, partiallyActive, ...rest }: LinkProps) => {
-const Link: React.FC<GatsbyLinkProps<{}>> = ({ to, activeClassName, partiallyActive, ...rest }) => {
+const Link: React.FC<LinkBaseProps & I18nGatsbyLinkProps> = ({ to, activeClassName, partiallyActive, ...rest }) => {
   // Tailor the following test to your environment.
   // This example assumes that any internal link (intended for Gatsby)
   // will start with exactly one slash, and that anything else is external.
@@ -20,7 +26,7 @@ const Link: React.FC<GatsbyLinkProps<{}>> = ({ to, activeClassName, partiallyAct
   if (internal) {
     return (
       <MuiLink
-        component={GatsbyLink}
+        component={FwdLink}
         to={to}
         activeClassName={activeClassName}
         partiallyActive={partiallyActive}
@@ -28,9 +34,7 @@ const Link: React.FC<GatsbyLinkProps<{}>> = ({ to, activeClassName, partiallyAct
       />
     );
   }
-  return (
-    <MuiLink href={to} {...rest} />
-  );
+  return <MuiLink href={to} {...rest} />;
 };
 
 export default Link;
