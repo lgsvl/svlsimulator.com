37b553719389233fdddc15541cf68deff1470e2e
Blake Stephens <blake.stephens@lge.com>
2020-10-20 18:32:07 -0700

    Fixed required theme prop

--- src/context/Theme.tsx
+++ src/context/Theme.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { ThemeProvider as MuiThemeProvider, ThemeProviderProps } from '@material-ui/core/styles';
 import { theme as defaultTheme } from '../styles/theme';
 
-export const ThemeProvider = ({ children, theme = defaultTheme }: ThemeProviderProps) => (
+export const ThemeProvider = ({ children, theme = defaultTheme }: Partial<ThemeProviderProps>) => (
   <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
 );
