5a31e2e2c152d12061b2578a97e7a7614fd93345
Blake Stephens <blake.stephens@lge.com>
2020-10-23 17:54:43 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-58d2649681dc067b2c56.js"
+    "url": "webpack-runtime-3938d212a4843c3c669d.js"
   },
   {
     "url": "framework-34b89739636bfb6cd5b9.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b74c0918bc06f007779ff861bf7216c4"
+    "revision": "84479ad45dfab93ccbf570a576b18c1d"
   },
   {
     "url": "static/webfonts/s/opensans/v18/mem8YaGs126MiZpBA-UFVZ0b.woff2"
