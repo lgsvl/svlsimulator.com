dfba87d9eb7a98648f3567039c39be447b6688fd
Blake Stephens <blake.stephens@lge.com>
2020-10-26 09:04:02 -0700

    Added mobile version of DocumentBox

--- src/components/DocumentBox.tsx
+++ src/components/DocumentBox.tsx
@@ -1,7 +1,7 @@
 import Box, { BoxProps } from '@material-ui/core/Box';
-import { withTheme } from '@material-ui/core/styles';
-// import { useTranslation } from 'src/hooks/useTranslations';
-// import Grid from '@material-ui/core/Grid';
+import { useTheme, withTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import Button from 'src/components/Button';
@@ -12,22 +12,37 @@ const StyledDocumentBox = withTheme(styled(Box)`
   border-color: ${({ theme }) => theme.palette.background.paper};
 `);
 
-const DocumentBox = ({ title, label, buttonText, ...rest }: BoxProps & { label?: string; buttonText?: string }) => (
-  <StyledDocumentBox display='flex' alignItems='center' border={1} my={4} p={2} borderRadius='borderRadius' {...rest}>
-    <Box>
-      <IconDocument />
-    </Box>
-    <Box flexGrow={1} ml={2}>
-      <Typography>{title}</Typography>
-      {label ? <Typography variant='body2'>{label}</Typography> : null}
-    </Box>
-    {buttonText ? (
-      <Box ml={2}>
-        <Button>{buttonText}</Button>
+const DocumentBox = ({ title, label, buttonText, ...rest }: BoxProps & { label?: string; buttonText?: string }) => {
+  const theme = useTheme();
+  const isXs = !useMediaQuery(theme.breakpoints.up('sm'));
+  const iconSize = isXs ? 42 : 80;
+  return (
+    <StyledDocumentBox
+      border={1}
+      my={4}
+      p={2}
+      alignItems='center'
+      wrap='nowrap'
+      display='flex'
+      flexDirection={isXs ? 'column' : 'row'}
+      borderRadius='borderRadius'
+      {...rest}
+    >
+      <Box>
+        <IconDocument height={iconSize} width={iconSize} />
       </Box>
-    ) : null}
-  </StyledDocumentBox>
-);
+      <Box flexGrow={1} ml={isXs ? 0 : 2} mt={isXs ? 1 : 0} textAlign={isXs ? 'center' : null}>
+        <Typography>{title}</Typography>
+        {label ? <Typography variant='body2'>{label}</Typography> : null}
+      </Box>
+      {buttonText ? (
+        <Box ml={isXs ? 0 : 2} mt={isXs ? 3 : 0}>
+          <Button>{buttonText}</Button>
+        </Box>
+      ) : null}
+    </StyledDocumentBox>
+  );
+};
 
 export default DocumentBox;
 export { DocumentBox };
