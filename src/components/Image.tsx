15dd82687e23fd78a4e72a325975247a4250f6f4
Blake Stephens <blake.stephens@lge.com>
2020-11-04 17:28:31 -0800

    Corrected a styling issue with overriding Image dimensions

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -7,10 +7,8 @@ export interface ImageProps extends BoxProps {
   src?: HTMLImageElement['src'];
 }
 
-const Image = withTheme(styled(({ src, ...rest }) => <Box {...rest} />)`
-  // height: 100%;
-  width: 100%;
-  min-height: 100px; // Arbitrarily assigned this to a non-zero value, so it's never invisible.
+// Arbitrarily assigned a non-zero value to min-height, so it's never invisible.
+const Image = withTheme(styled(({ src, ...rest }) => <Box height={1} width={1} minHeight='100px' {...rest} />)`
   background-image: url(${({ src }) => src});
   background-size: cover;
   background-position: center center;
