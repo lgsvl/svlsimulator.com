bf7c709c16a2ee5df15a6a0f22b67744cb4f704e
Blake Stephens <blake.stephens@lge.com>
2020-11-06 09:30:09 -0800

    Added Breadcrumbs component and refactored where translated titles come from

--- src/components/md/Layout.tsx
+++ src/components/md/Layout.tsx
@@ -4,15 +4,22 @@
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
-import { MDXProvider } from '@mdx-js/react';
+import { MDXProvider, MDXProviderProps } from '@mdx-js/react';
+import { PageProps } from 'gatsby';
 import React from 'react';
+import Breadcrumbs from 'src/components/Breadcrumbs';
 import Page from 'src/components/Page';
 import Subs from './Substitutions';
 
-export default function Layout({ children }: React.PropsWithChildren<typeof MDXProvider>) {
+export default function Layout({
+  children,
+  location,
+  pageContext
+}: React.PropsWithChildren<MDXProviderProps & PageProps>) {
   return (
     <MDXProvider components={Subs}>
       <Page>
+        <Breadcrumbs location={location} pageContext={pageContext} />
         <Container disableGutters maxWidth='md'>
           <Paper elevation={0}>
             <Box>{children}</Box>
