6f33a9f7647b57f7b0bab8805572b5f8ebf7c3ea
Blake Stephens <blake.stephens@lge.com>
2020-12-10 18:53:51 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5ac053c051cd9ef63418.js"
+    "url": "webpack-runtime-0b1c1436bfe7562a03cc.js"
   },
   {
-    "url": "framework-c34ea17250b9b4aae64e.js"
+    "url": "framework-004308d0699a3c3ebb06.js"
   },
   {
-    "url": "app-62620f62badcf9ed690c.js"
+    "url": "app-52117a23107b5d82d9bc.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f83bebbe5a09722241315dd6de094b22"
+    "revision": "b39b1e9128b220594a9cb9f156bfbf42"
   },
   {
     "url": "static/webfonts/s/opensans/v18/mem8YaGs126MiZpBA-UFVZ0b.woff2"
@@ -49,10 +49,10 @@ self.__precacheManifest = [
     "url": "static/webfonts/s/opensans/v18/mem5YaGs126MiZpBA-UN7rgOUuhp.woff2"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
   },
   {
-    "url": "polyfill-4f6310ee327c6ce3c4ce.js"
+    "url": "polyfill-01f981a48034094ff6be.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -148,7 +148,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-62620f62badcf9ed690c.js`))) {
+  if (!resources || !(await caches.match(`/app-52117a23107b5d82d9bc.js`))) {
     return await fetch(event.request)
   }
 
