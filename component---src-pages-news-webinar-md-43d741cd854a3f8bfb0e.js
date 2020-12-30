0eb79eac3eec020c4865ab920abdef6d9db11c7c
Djspaceg <Djspaceg@users.noreply.github.com>
2020-12-30 22:40:44 +0000

    deploy: ccc48df829c9b0386da25d9685910635050e569b

--- component---src-pages-news-webinar-md-43d741cd854a3f8bfb0e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[41],{ptQW:function(t,n,o){"use strict";o.r(n),o.d(n,"_frontmatter",(function(){return a})),o.d(n,"default",(function(){return s}));var e=o("8o2o"),r=(o("q1tI"),o("7ljp")),c=o("qmQJ"),a={},u={_frontmatter:a},p=c.a;function s(t){var n=t.components,o=Object(e.a)(t,["components"]);return Object(r.b)(p,Object.assign({},u,o,{components:n,mdxType:"MDXLayout"}),Object(r.b)("h2",null,"Safety for autonomous car"))}s.isMDXComponent=!0}}]);
-//# sourceMappingURL=component---src-pages-news-webinar-md-43d741cd854a3f8bfb0e.js.map
--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8471d2a41147b59dc0cb.js"
+    "url": "webpack-runtime-e6643815a02324938156.js"
   },
   {
-    "url": "framework-76768e28ddd4001143e6.js"
+    "url": "framework-ddc4a3be961269111282.js"
   },
   {
-    "url": "app-e1c3d8bb2dc7fe84637c.js"
+    "url": "app-73afac7580245c471cbe.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ad600e3c1e32beeb401f3b22498a686f"
+    "revision": "f506c5d894abd8cdad42c826cef11bf4"
   },
   {
     "url": "static/webfonts/s/opensans/v18/mem8YaGs126MiZpBA-UFVZ0b.woff2"
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-d3ee03be2ef85643f368.js"
   },
   {
-    "url": "polyfill-e752bf11bff6adbcde64.js"
+    "url": "polyfill-d1d25758dd3ab6a1aaef.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -148,7 +148,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-e1c3d8bb2dc7fe84637c.js`))) {
+  if (!resources || !(await caches.match(`/app-73afac7580245c471cbe.js`))) {
     return await fetch(event.request)
   }
 
