1fd025256fe57b0e7ac8e4e6a87ba5f29127b988
Jason Robitaille <jason.robitaille@lge.com>
2021-03-26 10:22:27 -0700

    Remove any lingering service workers

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,8 +2,23 @@
 const React = require('react');
 
 exports.onPreRenderHTML = ({ pathname, getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
+  // Remove any lingering service workers
+  headComponents.unshift(
+    <script dangerouslySetInnerHTML={{
+      __html: `
+        if (typeof navigator !== 'undefined' && navigator.serviceWorker && navigator.serviceWorker.getRegistrations) {
+          navigator.serviceWorker.getRegistrations().then(function(registrations) {
+            for(let registration of registrations) {
+              registration.unregister();
+            }
+          });
+        }
+      `
+    }} />
+  );
   if (pathname.endsWith('/404.html') && pathname !== '/docs/404.html') {
-    const headComponents = getHeadComponents();
+    // Add redirect to docs 404 page when URL originated from /docs/
     headComponents.unshift(
       <script dangerouslySetInnerHTML={{
         __html: `
@@ -13,6 +28,6 @@ exports.onPreRenderHTML = ({ pathname, getHeadComponents, replaceHeadComponents 
         `
       }} />
     );
-    replaceHeadComponents(headComponents);
   }
+  replaceHeadComponents(headComponents);
 }
